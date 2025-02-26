74f2b51b28d7e30229d2666b7c1a4a45f66ebf0a
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-02-26 20:34:56 +0100

    Time: 0 ms (100%), Space: 18.1 MB (10.57%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.py
@@ -1,13 +1,8 @@
 class Solution:
     def checkIfExist(self, arr: List[int]) -> bool:
         dct = {}
-        count = 0
+
         for i in range(len(arr)):
-            if arr[i]==0:
-                count+=1
-                if count == 2:
-                    return True
-                continue
             if arr[i]*2 in dct.keys() or (arr[i]%2==0 and arr[i]//2 in dct.keys()):
                 return True
             dct[arr[i]] = i
