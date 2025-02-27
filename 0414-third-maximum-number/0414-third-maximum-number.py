24ba4ff93202f3ce2eff8d03eea86c38cd6d8d34
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-02-27 14:55:15 +0100

    Time: 4 ms (15.19%), Space: 18.6 MB (75.5%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,21 +1,34 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
-        nums = sorted(nums)
-        nums = nums[::-1]
-        ans = nums[0]
+        firstMax = (-1, False)
+        secondMax = (-1, False)
+        thirdMax = (-1, False)
         
-        sec_max_seen = False
-        sec_max = ans
-        for i in range(1, len(nums)):
-            if nums[i]!=ans and not sec_max_seen:
-                sec_max = nums[i]
-                sec_max_seen = True
-                
-            elif nums[i]!=sec_max and sec_max_seen:
-                return nums[i]
-            else:
+        for num in nums:
+            # If current number is already stored, skip it.
+            if (firstMax[1] and firstMax[0] == num) or \
+                (secondMax[1] and secondMax[0] == num) or \
+                (thirdMax[1] and thirdMax[0] == num):
                 continue
-        return ans
             
-                
+            # If we never stored any variable in firstMax
+            # or curr num is bigger than firstMax, then curr num is the biggest number.
+            if not firstMax[1] or firstMax[0] <= num:
+                thirdMax = secondMax
+                secondMax = firstMax
+                firstMax = (num, True)
+            # If we never stored any variable in secondMax
+            # or curr num is bigger than secondMax, then curr num is 2nd biggest number.
+            elif not secondMax[1] or secondMax[0] <= num:
+                thirdMax = secondMax
+                secondMax = (num, True)
+            # If we never stored any variable in thirdMax
+            # or curr num is bigger than thirdMax, then curr num is 3rd biggest number.
+            elif not thirdMax[1] or thirdMax[0] <= num:
+                thirdMax = (num, True)
         
+        # If third max was never updated, it means we don't have 3 distinct numbers.
+        if not thirdMax[1]:
+            return firstMax[0]
+        
+        return thirdMax[0]
