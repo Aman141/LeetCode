1e42da7412282dc11ac72e9c4f1650f817bc1146
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-03-02 01:54:26 +0100

    Time: 0 ms (100%), Space: 18 MB (17.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,23 +1,17 @@
 class Solution:
-    def strip(self, s:str) -> list:
-        list_s = []
-        a = 0
-        b = 0
-        word = ""
-        for i in range(len(s)):
-            if s[i]== " ":
-                if word !="":
-                    list_s.append(word)
-                    word = ""
-                continue
-            else: 
-                word += s[i]  
-        if word!="":
-            list_s.append(word)
-        return list_s    
-
+ 
     def lengthOfLastWord(self, s: str) -> int:
-        list_s = self.strip(s)
-        last_word = list_s[-1]
-        return len(last_word)
+        length = 0
+        i = len(s) - 1
+
+        # Skip trailing spaces
+        while i >= 0 and s[i] == " ":
+            i -= 1
+
+        # Count the length of the last word
+        while i >= 0 and s[i] != " ":
+            length += 1
+            i -= 1
+
+        return length
         
