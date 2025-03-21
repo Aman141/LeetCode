cc1a24077c7f851c41de5305bde71ec58812193b
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-03-21 14:54:50 +0100

    Time: 0 ms (100%), Space: 17.7 MB (87.92%) - LeetHub

--- 1005-univalued-binary-tree/1005-univalued-binary-tree.py
+++ 1005-univalued-binary-tree/1005-univalued-binary-tree.py
@@ -8,12 +8,10 @@ class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
         if not root: return True
 
-        if root.left:
-            if root.left.val!= root.val:
-                return False
+        if root.left and root.left.val != root.val:
+            return False
+        if root.right and root.right.val != root.val:
+            return False
 
-        if root.right:        
-            if root.right.val!= root.val:
-                return False
-        
         return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
+        
