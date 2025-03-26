759d6f754421aac9bca01714c3ba1b09c2b672fe
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-03-26 16:27:27 +0100

    Time: 23 ms (58.98%), Space: 18.7 MB (45.98%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [False] * len(nums)
-        dp[0] = True
-        for i in range(len(nums)):
-            if dp[i]:
-                a = nums[i]
-                if a >= (len(dp)-i):
-                    return True
-
-                dp[i+1:i+a+1] = [True]*a
-
-        return dp[-1]        
+        max_reach = 0
+        for i, jump in enumerate(nums):
+            if i > max_reach:  # If current index is unreachable, return False
+                return False
+            max_reach = max(max_reach, i + jump)  # Update farthest reachable index
+            if max_reach >= len(nums) - 1:  # If last index is reachable, return True
+                return True
+        return True      
 
         
