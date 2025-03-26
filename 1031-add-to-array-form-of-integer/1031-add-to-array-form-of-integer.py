c98de2be6d4c2c9385a8d64239e32713c1937842
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-03-26 11:01:07 +0100

    Time: 16 ms (48.14%), Space: 18.4 MB (31.26%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.py
@@ -1,22 +1,15 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        carry = 0
-        for i in range(len(num)-1,-1,-1):
-            if k == 0 and carry==0:
-                break
-            t = num[i] + k%10 + carry    
-            num[i] = (t)%10
-            carry = t//10
-            k = k//10
+        i = len(num) - 1
+        while i >= 0 or k > 0:
+            if i >= 0:
+                k += num[i]  # Add the current digit of `num` to `k`
+                num[i] = k % 10  # Update `num[i]` with the last digit
+                i -= 1
+            else:
+                num.insert(0, k % 10)  # Insert remaining digits at the front
+            k //= 10  # Remove the last digit from `k`
 
-        if carry!=0 or k!=0:
-            ans = []
-            s = carry + k
-            print(s)
-            while(s):
-                print(s%10)
-                ans.append(s%10)
-                s = s//10
-            return ans[::-1] + num
+        return num
 
         return num
