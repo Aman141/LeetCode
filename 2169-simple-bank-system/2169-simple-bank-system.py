4390c70ada2b99c9f13ebc43fb41aee200c54735
Aman Kumar <43572255+Aman141@users.noreply.github.com>
2025-03-26 15:18:45 +0100

    Time: 23 ms (87.3%), Space: 47.8 MB (29.72%) - LeetHub

--- 2169-simple-bank-system/2169-simple-bank-system.py
+++ 2169-simple-bank-system/2169-simple-bank-system.py
@@ -1,32 +1,33 @@
 class Bank:
-
     def __init__(self, balance: List[int]):
         self.balance = balance
         self.n_account = len(balance)
-        
+
+    def is_valid_account(self, account: int) -> bool:
+        return 1 <= account <= self.n_account  # Validates if the account exists
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if account1 > self.n_account or account2 > self.n_account: return False
-        if money > self.balance[account1-1]: return False
-        else:
-            self.balance[account1-1] -= money
-            self.balance[account2-1] += money
-            return True    
+        if not (self.is_valid_account(account1) and self.is_valid_account(account2)):
+            return False
+        if self.balance[account1 - 1] < money:
+            return False
+        
+        self.balance[account1 - 1] -= money
+        self.balance[account2 - 1] += money
+        return True
 
     def deposit(self, account: int, money: int) -> bool:
-        if account > self.n_account: return False
-        else:
-            self.balance[account-1] += money
-            return True
-        
+        if not self.is_valid_account(account):
+            return False
+        self.balance[account - 1] += money
+        return True
 
     def withdraw(self, account: int, money: int) -> bool:
-        if account > self.n_account: return False
-        else:
-            if (self.balance[account-1]>= money):
-                self.balance[account-1] -= money
-                return True
-        return False        
+        if not self.is_valid_account(account) or self.balance[account - 1] < money:
+            return False
+        self.balance[account - 1] -= money
+        return True
+     
         
 
 
